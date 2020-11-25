e6ceb91b776f85a380759e57c14a273e4648bc3c
Cete4 <46322113+Cete4@users.noreply.github.com>
2020-11-24 22:29:11 -0600

    Finished it sort of oh well it looks average or something

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -15,6 +15,19 @@ function Contact() {
                     <Nav.Link id="navitem" href="Contact">Contact</Nav.Link>
                 </Nav>
             </Navbar>
+            <div id="contact">
+                <br></br>
+                <h3>Email: </h3>
+                <a>mbigleagle123@gmail.com</a>
+                <br></br>
+                <br></br>
+                <h3>Github: </h3>
+                <a href="https://github.com/Cete4" target="_blank">https://github.com/Cete4</a>
+                <br></br>
+                <br></br>
+                <h3>LinkedIn: </h3>
+                <a href="https://www.linkedin.com/in/micah-bigeagle-ba5b3b1ba/" target="_blank">https://www.linkedin.com/in/micah-bigeagle-ba5b3b1ba/</a>
+            </div>
             <Footer></Footer>
         </Container>
     );
